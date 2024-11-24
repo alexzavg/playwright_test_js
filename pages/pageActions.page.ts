c15b6ad312f8105ad69b0c4cd9d446a56a5334da
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2024-11-24 03:41:57 +0200

    upd

--- pages/pageActions.page.ts
+++ pages/pageActions.page.ts
@@ -3,67 +3,67 @@ import { test, step } from '../utils/envName'
 
 export class PageActions {
 
-  page: Page;
+  page: Page
 
   constructor(page: Page) {
-    this.page = page;
-  };
+    this.page = page
+  }
 
   public async openUrl(url: string) {
     await step(`Navigate to URL: '${url}'`, async () => {
-      await this.page.goto(url);
-    });
-  };
+      await this.page.goto(url)
+    })
+  }
 
   public async refreshPage() {
     await step(`Refresh page`, async () => {
-      await this.page.reload();
-      await this.page.waitForLoadState('load');
-      await this.page.waitForLoadState('domcontentloaded');
-      await this.page.waitForLoadState('networkidle');
-      await this.page.waitForTimeout(1000);
-    });
-  };
+      await this.page.reload()
+      await this.page.waitForLoadState('load')
+      await this.page.waitForLoadState('domcontentloaded')
+      await this.page.waitForLoadState('networkidle')
+      await this.page.waitForTimeout(1000)
+    })
+  }
 
   public async waitForUrl(url: string) {
     await step(`Wait until page URL is: '${url}'`, async () => {
-      await this.page.waitForURL(url);
-      await this.page.waitForLoadState('load');
-      await this.page.waitForLoadState('domcontentloaded');
-      await this.page.waitForLoadState('networkidle');
-      await this.page.waitForTimeout(500);
-    });
-  };
+      await this.page.waitForURL(url)
+      await this.page.waitForLoadState('load')
+      await this.page.waitForLoadState('domcontentloaded')
+      await this.page.waitForLoadState('networkidle')
+      await this.page.waitForTimeout(500)
+    })
+  }
 
   public async waitForElement(locator: string) {
     await step(`Wait until element: '${locator}' is visible`, async () => {
-      await this.page.waitForSelector(locator);
-    });
-  };
+      await this.page.waitForSelector(locator)
+    })
+  }
 
   public async waitForElementInvisible(locator: string) {
     await step(`Wait until element: '${locator}' is invisible`, async () => {
-      await expect(this.page.locator(locator)).not.toBeVisible();
-    });
-  };
+      await expect(this.page.locator(locator)).not.toBeVisible()
+    })
+  }
 
   public async waitForElementEnabled(locator: string) {
     await step(`Wait until element: '${locator}' doesn't have "disabled" attribute`, async () => {
-      await expect(this.page.locator(locator)).toBeEnabled();
-    });
-  };
+      await expect(this.page.locator(locator)).toBeEnabled()
+    })
+  }
 
   public async waitForElementEditable(locator: string) {
     await step(`Wait until element: '${locator}' doesn't have "readonly" property`, async () => {
-      await expect(this.page.locator(locator)).toBeEditable();
-    });
-  };
+      await expect(this.page.locator(locator)).toBeEditable()
+    })
+  }
 
   public async clickElement(locator: string) {
     await step(`Click element locator: '${locator}'`, async () => {
-      await this.page.locator(locator).click();
-    });
-  };
+      await this.page.locator(locator).click()
+    })
+  }
 
   public async clickElementForce(locator: string) {
     await step(`Click element locator: '${locator}'`, async () => {
@@ -71,147 +71,147 @@ export class PageActions {
         button: 'middle',
         clickCount: 10,
         delay: 250
-      });
-    });
-  };
+      })
+    })
+  }
 
   public async clickElementCentre(locator: string) {
     await step(`Click element locator: '${locator}'`, async () => {
-      const selector = this.page.locator(locator);
-      const box = await selector.boundingBox();
-      // await selector.scrollIntoViewIfNeeded();
-      await this.page.mouse.click(box!.x + box!.width / 2, box!.y + box!.height / 2);
-    });
-  };
+      const selector = this.page.locator(locator)
+      const box = await selector.boundingBox()
+      // await selector.scrollIntoViewIfNeeded()
+      await this.page.mouse.click(box!.x + box!.width / 2, box!.y + box!.height / 2)
+    })
+  }
 
   public async fillElement(locator: string, text: string) {
     await step(`Fill element locator: '${locator}' with text: '${text}'`, async () => {
-      const selector = this.page.locator(locator);
-      await selector.fill(text);
-    });
-  };
+      const selector = this.page.locator(locator)
+      await selector.fill(text)
+    })
+  }
 
   public async pressKey(key: string) {
      await step(`Press '${key}' key`, async () => {
-      await this.page.keyboard.press(key);
-    });
-  };
+      await this.page.keyboard.press(key)
+    })
+  }
 
   public async checkElementAttribute(locator: string, attr: string, value: string) {
     await step(`Get attribute: '${attr}' from element: '${locator}'`, async () => {
-      const selector = this.page.locator(locator);
-      const attribute = await selector.getAttribute(attr);
-      await expect(attribute).toEqual(value);
-    });
-  };
+      const selector = this.page.locator(locator)
+      const attribute = await selector.getAttribute(attr)
+      await expect(attribute).toEqual(value)
+    })
+  }
 
   public async waitForElementAttribute(locator: string, attr: string, value: string, timeout = 60000) {
     await step(`Check element '${locator}' for attribute '${attr}'`, async () => {
-      const selector = this.page.locator(locator);
+      const selector = this.page.locator(locator)
       
       await this.page.waitForFunction(
         ([locator, attr, value]) => {
-          const element = document.querySelector(locator);
+          const element = document.querySelector(locator)
           if (element) {
-            return element.getAttribute(attr) === value;
+            return element.getAttribute(attr) === value
           }
-          return false;
+          return false
         },
         [locator, attr, value],
         { timeout }
-      );
+      )
 
-      const attribute = await selector.getAttribute(attr);
-      expect(attribute).toContain(value);
-    });
+      const attribute = await selector.getAttribute(attr)
+      expect(attribute).toContain(value)
+    })
   }
 
   public async checkElementText(locator: string, text: string) {
     await step(`Check text from element with locator: '${locator}'`, async () => {
-      const selector = this.page.locator(locator);
-      const textValue = await selector.textContent();
-      await expect(textValue).toEqual(text);
-    });
-  };
+      const selector = this.page.locator(locator)
+      const textValue = await selector.textContent()
+      await expect(textValue).toEqual(text)
+    })
+  }
 
   public async getElementText(locator: string) {
-    let text;
+    let text
     await step(`Get text from element with locator: '${locator}'`, async () => {
-      const selector = this.page.locator(locator);
-      text = await selector.textContent();
-    });
-    return text as string;
-  };
+      const selector = this.page.locator(locator)
+      text = await selector.textContent()
+    })
+    return text as string
+  }
 
   public async selectDropdownOption(locator: string, option: string) {
     await step(`Select '${option}' from dropdown with locator '${locator}'`, async () => {
-      const dropdown = await this.page.$(locator);
-      await dropdown!.click();
+      const dropdown = await this.page.$(locator)
+      await dropdown!.click()
   
-      const optionElement = await this.page.$(`${locator} option[value="${option}"]`);
-      await optionElement!.click();
-    });
-  };
+      const optionElement = await this.page.$(`${locator} option[value="${option}"]`)
+      await optionElement!.click()
+    })
+  }
 
   public async checkCheckbox(locator: string) {
     await step(`Check checkbox with locator '${locator}'`, async () => {
-      await this.page.locator(locator).check();
-      await expect(this.page.locator(locator)).toBeChecked();
-    });
-  };
+      await this.page.locator(locator).check()
+      await expect(this.page.locator(locator)).toBeChecked()
+    })
+  }
 
   public async isCheckboxChecked(locator: string) {
     await step(`Check checkbox with locator '${locator}'`, async () => {
-      await expect(this.page.locator(locator)).toBeChecked();
-    });
-  };
+      await expect(this.page.locator(locator)).toBeChecked()
+    })
+  }
 
   public async uncheckCheckbox(locator: string) {
     await step(`Uncheck checkbox with locator '${locator}'`, async () => {
-      await this.page.locator(locator).uncheck();
-      await expect(this.page.locator(locator)).not.toBeChecked();
-    });
-  };
+      await this.page.locator(locator).uncheck()
+      await expect(this.page.locator(locator)).not.toBeChecked()
+    })
+  }
 
   public async isCheckboxUnchecked(locator: string) {
     await step(`Check checkbox with locator '${locator}'`, async () => {
-      await expect(this.page.locator(locator)).not.toBeChecked();
-    });
-  };
+      await expect(this.page.locator(locator)).not.toBeChecked()
+    })
+  }
 
   public async getCookie(URL: string, cookieName: string){
-    var cookie = await this.page.context().cookies(URL);
-    //console.log("cookie text: " + JSON.stringify(cookie));
+    var cookie = await this.page.context().cookies(URL)
+    //console.log("cookie text: " + JSON.stringify(cookie))
 
-    var lengthCookie = cookie.length;
-    var valueOfCookie;
+    var lengthCookie = cookie.length
+    var valueOfCookie
 
     for (var step = 0; step < lengthCookie; step++) {
       if(cookie[step]['name'] === cookieName){
-        valueOfCookie = cookie[step]['value'];
-        //console.log(`Cookie: ${cookieName}=${valueOfCookie}`);
-      };
+        valueOfCookie = cookie[step]['value']
+        //console.log(`Cookie: ${cookieName}=${valueOfCookie}`)
+      }
     }
 
-    return `${cookieName}=${valueOfCookie}`;
-  };
+    return `${cookieName}=${valueOfCookie}`
+  }
 
   public async setCookie(cookieName: string, cookieValue: string) {
     // Set the cookie
     await this.page.evaluate(({ name, value }) => {
-      document.cookie = `${name}=${value}`;
-    }, { name: cookieName, value: cookieValue });
+      document.cookie = `${name}=${value}`
+    }, { name: cookieName, value: cookieValue })
   
     // Check if the cookie was set
     const cookieSet = await this.page.evaluate((name) => {
-      const cookies = document.cookie;
-      return cookies.includes(name);
-    }, cookieName);
+      const cookies = document.cookie
+      return cookies.includes(name)
+    }, cookieName)
   
     // Throw an error if the cookie wasn't set
     if (!cookieSet) {
-      throw new Error(`Cookie "${cookieName}" was not set.`);
+      throw new Error(`Cookie "${cookieName}" was not set.`)
     }
-  };
+  }
 
-};
+}
